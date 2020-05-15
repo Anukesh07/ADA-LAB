9096e8a335ace86cb3c2a4f98e40338df83b4b82
Anukesh07 <53857067+Anukesh07@users.noreply.github.com>
2020-05-16 00:36:25 +0530

    Create dfs.c

---
+++ dfs.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int visit[20],n,adj[20][20],s,count=0;
+
+void dfs(int x)
+{
+  int y;
+  visit[x]=1;
+  count++;
+  for(y=1;y<=n;y++)
+  {
+    if((adj[x][y]==1) && (visit[y]==0))
+    {
+      dfs(y);
+    }
+  }
+}
+
+void main()
+{
+  int x,y;
+  printf("Enter the number of vertices:\n");
+  scanf("%d",&n);
+  printf("Enter the adjacency matrix:\n");
+  for(x=1;x<=n;x++)
+  {
+    for(y=1;y<=n;y++)
+    {
+      scanf("%d",&adj[x][y]);
+    }
+  }
+  for(x=1;x<=n;x++)
+  {
+      visit[x]=0;
+  }
+    dfs(1);
+  if(count==n)
+  {
+   printf("\nThe graph is connected");
+  }
+  else
+  {
+   printf("\nThe graph is not connected");
+  }
+}
